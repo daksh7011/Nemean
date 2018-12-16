549e3337f55af96f10a510514b91e42d27c9555d
Daksh <daksh@technowolf.in>
2018-12-16 20:58:38 +0530

    replaced var with let
    Signed-off-by: Daksh <daksh@technowolf.in>

--- js/main.js
+++ js/main.js
@@ -5,19 +5,26 @@
  *
  */
 
-"use strict"
+/*
+ * Open Source Project made by Daksh (daksh7011.com)
+ * Do NOT remove this excerpt even if you fork this or use it by any means.
+ * This project is Licenced under GNU GENERAL PUBLIC LICENSE Version 3
+ *
+ */
+
+"use strict";
 $(window).on("load", function() {
     function countDown() {
-        var today = new Date();
-        var comingsoonDate = new Date("December 31,2018 23:59:58");
-        var currentTime = today.getTime();
-        var countdownTime = comingsoonDate.getTime();
-        var actualTime = countdownTime - currentTime;
+        let today = new Date();
+        let comingsoonDate = new Date("December 31,2018 23:59:58");
+        let currentTime = today.getTime();
+        let countdownTime = comingsoonDate.getTime();
+        let actualTime = countdownTime - currentTime;
 
-        var sec = Math.floor(actualTime / 1000);
-        var min = Math.floor(sec / 60);
-        var hrs = Math.floor(min / 60);
-        var days = Math.floor(hrs / 24);
+        let sec = Math.floor(actualTime / 1000);
+        let min = Math.floor(sec / 60);
+        let hrs = Math.floor(min / 60);
+        let days = Math.floor(hrs / 24);
 
         hrs %= 24;
         min %= 60;
@@ -28,11 +35,11 @@ $(window).on("load", function() {
         min = (min < 10) ? "0" + min : min;
         sec = (sec < 10) ? "0" + sec : sec;
 
-        var elTimeCounter = $('.time-counter');
-        var elDays = $('.days', elTimeCounter);
-        var elHours = $('.hours', elTimeCounter);
-        var elMinutes = $('.minutes', elTimeCounter);
-        var elSeconds = $('.seconds', elTimeCounter);
+        let elTimeCounter = $('.time-counter');
+        let elDays = $('.days', elTimeCounter);
+        let elHours = $('.hours', elTimeCounter);
+        let elMinutes = $('.minutes', elTimeCounter);
+        let elSeconds = $('.seconds', elTimeCounter);
 
         $('.num', elDays).html(days);
         $('.num', elHours).html(hrs);
